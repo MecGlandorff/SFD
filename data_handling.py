d26a48afb4655843c63a4234ec5c41be415389c5
MecGlandorff <mpjglandorff@gmail.com>
2024-12-05 01:16:02 +0200

    data_handling merging data methods and functions

--- data_handling.py
+++ data_handling.py
@@ -12,6 +12,7 @@ class DataHandler:
         """
         self.file_paths = config['data_paths']
         self.data = {}
+        self.merged_data = None
         self.logger = logging.getLogger('DataHandler')
 
     def load_data(self):
@@ -19,7 +20,7 @@ class DataHandler:
         Load data from file paths
         """
         try:
-            self.logger.info('Loading data...')
+            self.logger.info('loading data...')
             for name, path in self.file_paths.items():
                 self.data[name] = pd.read_csv(path)
                 self.logger.info(f'{name} loaded successfully from {path}')
@@ -33,17 +34,42 @@ class DataHandler:
         because missing values are handled incorrectly!
         """
         try:
-            self.logger.info('Preprocessing data...')
+            # Loggin statement
+            self.logger.info('preprocessing data...')
             # Handle missing values
             for df_name, df in self.data.items():
                 self.data[df_name] = df.dropna()
-                self.logger.info(f'{df_name}: Missing values handled.')
+                
+                # Loggin statement
+                self.logger.info(f'{df_name}: missing values handled')
             
             # Below add the other preprocessing steps as merging etc. This module is totally useless still because the 
             # way the files are now composed doesnt do shit :0:). 
+
+            # Merge merge merggee
+            self.merge_data()
+
         except Exception as e:
-            self.logger.error(f'Error during preprocessing: {e}')
+            self.logger.error(f'error during preprocessing: {e}')
             raise
+
+    # Merge data method
+    def merge_data(self):
+        """ Merges datasets pandas"""
+
+        try:
+            # Loggin' statement 
+            self.logger.info("merging data")
+
+            # Merge the segments. Later look into how and which for now just generalized method
+            df_segmented_data = self.data('train_segmented')
+            df_meta_train = self.data('meta_train_clean')
+
+            # Loggin' statement
+            self.logger.info()
+            merged_df = pd.merge(df_segmented_data, df_meta_train, on=[# forgot all the columns and too late for tomorrow point of continuation])
+
+
 
     def get_data(self):
         """
